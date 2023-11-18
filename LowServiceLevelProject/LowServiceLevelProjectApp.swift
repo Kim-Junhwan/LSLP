32f5a28acd0d505890869fb44f7c8a026bdea191
Kim junhwan <tazanboy1@naver.com>
2023-11-18 12:41:22 +0900

    프로젝트 생성

---
+++ LowServiceLevelProject/LowServiceLevelProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  LowServiceLevelProjectApp.swift
+//  LowServiceLevelProject
+//
+//  Created by JunHwan Kim on 2023/11/18.
+//
+
+import SwiftUI
+
+@main
+struct LowServiceLevelProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
