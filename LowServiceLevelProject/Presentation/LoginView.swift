1bacedca864abfa867b703037c5f6cf4f68a61d2
Kim junhwan <tazanboy1@naver.com>
2023-11-19 14:23:33 +0900

    [feat] 회원가입 버튼 클릭시 회원가입뷰 이동

--- LowServiceLevelProject/Presentation/LoginView.swift
+++ LowServiceLevelProject/Presentation/LoginView.swift
@@ -9,22 +9,42 @@ import SwiftUI
 
 struct LoginView: View {
     
-    @State var id: String = ""
-    @State var password: String = ""
+    @State private var id: String = ""
+    @State private var password: String = ""
+    @FocusState private var isFocus: Bool
+    @State private var presentRegisterView: Bool = false
     
     var body: some View {
-        VStack {
-            TextField(text: $id, prompt: Text("아이디")) {
-                
-            }
-            .padding(10)
-            .focusHightLight()
-            
-            SecureField(text: $password, prompt: Text("비밀번호")) {}
+            VStack(alignment: .center, spacing: 20) {
+                TextField(text: $id, prompt: Text("아이디")) {
+                    
+                }
                 .padding(10)
                 .focusHightLight()
-        }
-        
+                .focused($isFocus)
+                
+                SecureField(text: $password, prompt: Text("비밀번호")) {}
+                    .padding(10)
+                    .focusHightLight()
+                    .focused($isFocus)
+                Button("로그인") {}
+                Button("회원가입") {
+                    presentRegisterView = true
+                }
+            }
+            .fullScreenCover(isPresented: $presentRegisterView, content: {
+                RegisterView()
+            })
+            .onAppear()
+            .frame(width: 250)
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    Button("Done") {
+                        isFocus = false
+                    }
+                }
+            }
     }
 }
 
---
+++ LowServiceLevelProject/Presentation/RegisterView.swift
@@ -0,0 +1,18 @@
+//
+//  RegisterView.swift
+//  LowServiceLevelProject
+//
+//  Created by JunHwan Kim on 2023/11/19.
+//
+
+import SwiftUI
+
+struct RegisterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RegisterView()
+}
