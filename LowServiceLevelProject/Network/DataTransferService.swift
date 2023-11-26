6c3200a5a4879b7c09ae6f3f2f86d436b109529c
Kim junhwan <tazanboy1@naver.com>
2023-11-27 02:41:11 +0900

    [feat] ErrorHandling하는 request 구현

--- LowServiceLevelProject/Network/DataTransferService.swift
+++ LowServiceLevelProject/Network/DataTransferService.swift
@@ -15,10 +15,36 @@ enum DataTransferServiceError: Error {
 
 final class DataTransferService {
     private let networkService: NetworkService
+    private let defaultResponseErrorHandler: ResponseErrorHandler?
     private let jsonDecoder: JSONDecoder = JSONDecoder()
     
-    init(networkService: NetworkService) {
+    init(networkService: NetworkService, defaultResponseHandler: ResponseErrorHandler? = nil) {
         self.networkService = networkService
+        self.defaultResponseErrorHandler = defaultResponseHandler
+    }
+    
+    func request<T: Decodable,U: Networable, R: ResponseErrorHandler>(endpoint: U , errorhandler: R, completion: @escaping (Result<U.responseType, Error>) -> Void) where T == U.responseType{
+        networkService.request(endPoint: endpoint) { result in
+            switch result {
+            case .success(let data):
+                let fetchData: Result<U.responseType, Error> = self.decode(data: data)
+                completion(fetchData)
+            case .failure(let error):
+                switch error {
+                case .responseError(statusCode: let statusCode, data: _):
+                    if let responseHandler = self.defaultResponseErrorHandler {
+                        responseHandler.mappingStatusCode(statusCode: statusCode)?.errorHandling(endPoint: endpoint, networkService: self.networkService, completion: { result in
+                            
+                        })
+                    }
+                    errorhandler.mappingStatusCode(statusCode: statusCode)?.errorHandling(endPoint: endpoint, networkService: self.networkService, completion: { result in
+                        
+                    })
+                case .networkError(error: _), .url:
+                    completion(.failure(error))
+                }
+            }
+        }
     }
     
     func request<T: Decodable,U: Networable>(endpoint: U, completion: @escaping (Result<U.responseType, Error>) -> Void) where T == U.responseType{
