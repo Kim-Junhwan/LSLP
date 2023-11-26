8caf5b001c0c219b7e78c605af158b1957f748fe
Kim junhwan <tazanboy1@naver.com>
2023-11-26 16:39:55 +0900

    [feat] 회원가입 EndPoint 정의

--- LowServiceLevelProject/API/APIs.swift
+++ LowServiceLevelProject/API/APIs.swift
@@ -8,42 +8,7 @@
 import Foundation
 
 enum LSLPAPIEndpoints {
-    case register(_ request: RegisterRequest)
-    
-    var baseURL: URL {
-        guard let url = URL(string: APIKey.baseURL) else { fatalError("failed make baseurl") }
-        return url
-    }
-    
-    var baseHeader: [String: String] {
-        var header: [String: String] = [:]
-        header["SesacKey"] = ""
-        switch self {
-        case .register(_):
-            header["Content-Type"] = "application/json"
-        }
-        
-        return header
-    }
-    
-    var path: String {
-        switch self {
-        case .register(_):
-            return "/join"
-        }
-    }
-    
-    var method: HTTPMethodType {
-        switch self {
-        case .register(_):
-            return .POST
-        }
-    }
-    
-    func makeEndpoint() -> any Networable {
-        switch self {
-        case .register(let registerInfo):
-            return EndPoint<RegisterResponse>(path: path, method: method, bodyParameter: registerInfo)
-        }
+    static func registerService(request: RegisterRequest) -> EndPoint<RegisterResponse> {
+        return EndPoint(path: "join", method: .POST, header: ["Content-Type":"application/json"], bodyParameter: request)
     }
 }
