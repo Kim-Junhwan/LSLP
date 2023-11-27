41389631569fd5c63f3618b3cb1f06203610076c
Kim junhwan <tazanboy1@naver.com>
2023-11-27 23:57:46 +0900

    [feat] ResponseErrorHandler에 errorHandling, retry optional func으로 변경

--- LowServiceLevelProject/Network/ResponseErrorHandler.swift
+++ LowServiceLevelProject/Network/ResponseErrorHandler.swift
@@ -7,9 +7,14 @@
 
 import Foundation
 
-protocol ResponseErrorHandler {
-    func errorHandling(endPoint: Requestable, networkService: NetworkService, completion: @escaping (Result<Data, NetworkError>) -> Void)
+protocol ResponseErrorHandler: LocalizedError {
+    func errorHandling(endPoint: Requestable, networkService: NetworkService, completion: @escaping (Result<Data, Error>) -> Void)
     func mappingStatusCode(statusCode: Int) -> Self?
-    
     func retry(completion: @escaping(Result<Data, NetworkError>) -> Void)
+}
+
+extension ResponseErrorHandler {
+    func errorHandling(endPoint: Requestable, networkService: NetworkService, completion: @escaping (Result<Data, Error>) -> Void) {}
+    
+    func retry(completion: @escaping(Result<Data, NetworkError>) -> Void) {}
 }
