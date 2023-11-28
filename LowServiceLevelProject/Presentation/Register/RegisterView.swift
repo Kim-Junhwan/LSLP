41c6f0c04b0ea10dc1851531bb0cd3e13690a260
Kim junhwan <tazanboy1@naver.com>
2023-11-28 22:47:39 +0900

    [feat] RegisterView에 로딩 뷰 적용

--- LowServiceLevelProject/Presentation/Register/RegisterView.swift
+++ LowServiceLevelProject/Presentation/Register/RegisterView.swift
@@ -11,33 +11,55 @@ struct RegisterView: View {
     
     @StateObject var viewModel = RegisterViewModel()
     @State private var showSheet = false
+    @State private var showErrorAlert = false
+    @State private var showAlert = false
+    
     
     var body: some View {
         NavigationStack {
-            VStack {
-                HStack {
-                    TextField("이메일", text: $viewModel.email, prompt: Text("이메일"))
-                        .focusHightLight()
-                    Button("중복 확인") {
-                        
+            ZStack {
+                VStack {
+                    HStack {
+                        TextField("이메일", text: $viewModel.email, prompt: Text("이메일"))
+                            .focusHightLight()
+                        Button("중복 확인") {
+                            viewModel.validateEmail { error in
+                                if error != nil {
+                                    showErrorAlert = true
+                                    return
+                                }
+                                showAlert = true
+                            }
+                        }
+                        .disabled(viewModel.emailEmpty)
+                        .alert("에러", isPresented: $showErrorAlert) {
+                            Button("확인"){}
+                        } message: {
+                            Text(viewModel.errorDescription)
+                        }
+                        .alert("사용가능한 이메일", isPresented: $showAlert) {
+                            Button("확인"){}
+                        }
                     }
+                    TextField("비밀번호", text: $viewModel.password, prompt: Text("비밀번호"))
+                        .focusHightLight()
+                    TextField("닉네임", text: $viewModel.nick, prompt: Text("닉네임"))
+                        .focusHightLight()
+                    TextField("핸드폰 번호", text: $viewModel.phone, prompt: Text("핸드폰 번호"))
+                        .focusHightLight()
+                    DatePicker("생일", selection: $viewModel.birthDay, displayedComponents: [.date])
                 }
-                TextField("비밀번호", text: $viewModel.password, prompt: Text("비밀번호"))
-                    .focusHightLight()
-                TextField("닉네임", text: $viewModel.nick, prompt: Text("닉네임"))
-                    .focusHightLight()
-                TextField("핸드폰 번호", text: $viewModel.phone, prompt: Text("핸드폰 번호"))
-                    .focusHightLight()
-                DatePicker("생일", selection: $viewModel.birthDay, displayedComponents: [.date])
-            }
-            .frame(width: 350)
+                .frame(width: 350)
                 .toolbar {
                     ToolbarItem(placement: .topBarTrailing) {
                         Button("회원가입") {
-                            viewModel.register()
+                            //viewModel.register()
                         }
                     }
                 }
+                LoadingView(isShow: $viewModel.isLoading)
+                    .ignoresSafeArea()
+            }
         }
     }
 }
--- LowServiceLevelProject/Presentation/Register/RegisterViewModel.swift
+++ LowServiceLevelProject/Presentation/Register/RegisterViewModel.swift
@@ -14,6 +14,12 @@ final class RegisterViewModel: ObservableObject {
     @Published var nick: String = ""
     @Published var phone: String = ""
     @Published var birthDay: Date = Date()
+    @Published var isLoading: Bool = false
+    var errorDescription: String = ""
+    
+    var emailEmpty: Bool {
+        return email.isEmpty
+    }
     
     let repository: AuthorizationRepository = DefaultAuthRepository(dataTransferService: DataTransferService(networkService: DefaultNetworkService(config: APINetworkConfigs.registerConfig)))
     
@@ -21,7 +27,37 @@ final class RegisterViewModel: ObservableObject {
 //        self.repository = repository
 //    }
     
-    func register() {
-        repository.register(request: .init(email: email, password: password, nick: nick, phoneNumber: phone, birthDay: birthDay))
+    func register(completion: @escaping (Error?)-> Void) {
+        changeLoadingValue(true)
+        repository.register(request: .init(email: email, password: password, nick: nick, phoneNumber: phone, birthDay: birthDay.description)) { result in
+            switch result {
+            case .success(_):
+                completion(nil)
+            case .failure(let failure):
+                self.errorDescription = failure.localizedDescription
+                completion(failure)
+            }
+            self.changeLoadingValue(false)
+        }
+    }
+    
+    func validateEmail(completion: @escaping (Error?)-> Void) {
+        changeLoadingValue(true)
+        repository.validateEmail(request: .init(email: email)) { result in
+            switch result {
+            case .success(_):
+                completion(nil)
+            case .failure(let failure):
+                self.errorDescription = failure.localizedDescription
+                completion(failure)
+            }
+            self.changeLoadingValue(false)
+        }
+    }
+    
+    private func changeLoadingValue(_ bool: Bool) {
+        DispatchQueue.main.async {
+            self.isLoading = bool
+        }
     }
 }
