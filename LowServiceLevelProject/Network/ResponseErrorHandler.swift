4a75819b7f2f94f7bb3ebbd36ba0fb12d658f2a5
Kim junhwan <tazanboy1@naver.com>
2023-11-29 20:42:31 +0900

    [feat] ResponseErrorHandler 에러 정의

--- LowServiceLevelProject/Network/ResponseErrorHandler.swift
+++ LowServiceLevelProject/Network/ResponseErrorHandler.swift
@@ -7,14 +7,17 @@
 
 import Foundation
 
-protocol ResponseErrorHandler: LocalizedError {
-    func errorHandling(endPoint: Requestable, networkService: NetworkService, completion: @escaping (Result<Data, Error>) -> Void)
-    func mappingStatusCode(statusCode: Int) -> Self?
-    func retry(completion: @escaping(Result<Data, NetworkError>) -> Void)
+enum RetryResult {
+    case notRetry(error: Error)
+    case retry
+}
+
+protocol ResponseErrorHandler {
+    associatedtype ResponseError: Error
+    func mappingStatusCode(statusCode: Int) -> ResponseError?
+    func retry(reseponseError: ResponseError ,completion: @escaping(RetryResult) -> Void)
 }
 
 extension ResponseErrorHandler {
     func errorHandling(endPoint: Requestable, networkService: NetworkService, completion: @escaping (Result<Data, Error>) -> Void) {}
-    
-    func retry(completion: @escaping(Result<Data, NetworkError>) -> Void) {}
 }
