77458bd032a063af7d6ae963710f448d1be6b4d2
Kim junhwan <tazanboy1@naver.com>
2023-12-01 15:56:39 +0900

    [feat] retry 로직 구현

--- LowServiceLevelProject/Network/DataTransferService.swift
+++ LowServiceLevelProject/Network/DataTransferService.swift
@@ -13,17 +13,66 @@ enum DataTransferServiceError: Error {
     case networkError(error: NetworkError)
 }
 
-final class DataTransferService {
+
+final class DataTransferService<DefaultErrorHandler: ResponseErrorHandler> {
     private let networkService: NetworkService
-    private let defaultResponseErrorHandler: ResponseErrorHandler?
+    private let defaultResponseErrorHandler: DefaultErrorHandler
     private let jsonDecoder: JSONDecoder = JSONDecoder()
     
-    init(networkService: NetworkService, defaultResponseHandler: ResponseErrorHandler? = nil) {
+    init(networkService: NetworkService, defaultResponseHandler: DefaultErrorHandler) {
         self.networkService = networkService
         self.defaultResponseErrorHandler = defaultResponseHandler
     }
     
-    func request<T: Decodable,U: Networable, R: ResponseErrorHandler>(endpoint: U , errorhandler: R, completion: @escaping (Result<U.responseType, Error>) -> Void) where T == U.responseType{
+    private func request<T: Decodable>(count: Int, maxCount: Int,endpoint: Requestable, endpointResponseHandler: (some ResponseErrorHandler)?,  completion: @escaping (Result<T, Error>) -> Void){
+        
+        let responseClosure: (RetryResult) -> Void = { retryResult in
+            switch retryResult {
+            case .notRetry(let error):
+                completion(.failure(error))
+            case .retry(let endpoint, let maxCount):
+                self.request(count: count+1, maxCount: maxCount, endpoint: endpoint, endpointResponseHandler: endpointResponseHandler) { result in
+                    completion(result)
+                }
+            }
+        }
+        
+        networkService.request(endPoint: endpoint) { result in
+            switch result {
+            case .success(let success):
+                let fetchData: Result<T, Error> = self.decode(data: success)
+                completion(fetchData)
+            case .failure(let failure):
+                switch failure {
+                case .responseError(statusCode: let statusCode, data: _):
+                    if count >= maxCount {
+                        completion(.failure(failure))
+                    } else {
+                        if let defaultStatus = self.defaultResponseErrorHandler.mappingStatusCode(statusCode: statusCode) {
+                            defaultStatus.retry(endpoint: endpoint, completion: responseClosure)
+                            return
+                        } else {
+                            endpointResponseHandler?.mappingStatusCode(statusCode: statusCode)?.retry(endpoint: endpoint, completion: responseClosure)
+                        }
+                    }
+                case .networkError(error: _), .url:
+                    completion(.failure(failure))
+                }
+            }
+        }
+    }
+    
+    func request<T: Decodable,U: Networable>(endpoint: U, endpointResponseHandler: (some ResponseErrorHandler)?, completion: @escaping (Result<U.responseType, Error>) -> Void) where T == U.responseType{
+        let responseResultClosure: (RetryResult) -> Void = { result in
+            switch result {
+            case .notRetry(let error):
+                completion(.failure(error))
+            case .retry(let end, let maxCount):
+                self.request(count: 0, maxCount: maxCount, endpoint: end, endpointResponseHandler: endpointResponseHandler) { result in
+                    completion(result)
+                }
+            }
+        }
         networkService.request(endPoint: endpoint) { result in
             switch result {
             case .success(let data):
@@ -32,14 +81,12 @@ final class DataTransferService {
             case .failure(let error):
                 switch error {
                 case .responseError(statusCode: let statusCode, data: _):
-                    if let responseHandler = self.defaultResponseErrorHandler {
-                        responseHandler.mappingStatusCode(statusCode: statusCode)?.errorHandling(endPoint: endpoint, networkService: self.networkService, completion: { result in
-                            
-                        })
+                    if let defaultStatus = self.defaultResponseErrorHandler.mappingStatusCode(statusCode: statusCode) {
+                        defaultStatus.retry(endpoint: endpoint, completion: responseResultClosure)
+                        return
+                    } else {
+                        endpointResponseHandler?.mappingStatusCode(statusCode: statusCode)?.retry(endpoint: endpoint, completion: responseResultClosure)
                     }
-                    errorhandler.mappingStatusCode(statusCode: statusCode)?.errorHandling(endPoint: endpoint, networkService: self.networkService, completion: { result in
-                        
-                    })
                 case .networkError(error: _), .url:
                     completion(.failure(error))
                 }
@@ -47,7 +94,9 @@ final class DataTransferService {
         }
     }
     
-    func request<T: Decodable,U: Networable>(endpoint: U, completion: @escaping (Result<U.responseType, Error>) -> Void) where T == U.responseType{
+    
+    
+    func request<T: Decodable,U: Networable>(endpoint: U,completion: @escaping (Result<U.responseType, Error>) -> Void) where T == U.responseType{
         networkService.request(endPoint: endpoint) { result in
             switch result {
             case .success(let data):
