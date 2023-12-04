fba59830e9abb4f0dd3aed65333da930874d7814
Kim junhwan <tazanboy1@naver.com>
2023-12-04 20:18:13 +0900

    [feat] KeyChain 키값 정의

---
+++ LowServiceLevelProject/Data/Keychain/KeychainAuthorizNameSpace.swift
@@ -0,0 +1,13 @@
+//
+//  AuthorizNameSpace.swift
+//  LowServiceLevelProject
+//
+//  Created by JunHwan Kim on 2023/12/04.
+//
+
+import Foundation
+
+enum KeychainAuthorizNameSpace {
+    static let refreshToken: String = "RefreshToken"
+    static let accesshToken: String = "AccessToken"
+}
