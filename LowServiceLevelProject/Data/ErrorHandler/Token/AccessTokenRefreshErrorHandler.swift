04c12cfc519863bbf7b05aadf89c2d283dbbf9d4
Kim junhwan <tazanboy1@naver.com>
2023-12-08 14:32:29 +0900

    [feat] 액세스 토큰 재발급 에러 핸들러 구현

---
+++ LowServiceLevelProject/Data/ErrorHandler/Token/AccessTokenRefreshErrorHandler.swift
@@ -0,0 +1,32 @@
+//
+//  RefreshTokenErrorHandler.swift
+//  LowServiceLevelProject
+//
+//  Created by JunHwan Kim on 2023/12/08.
+//
+
+import Foundation
+
+struct AccessTokenRefreshErrorHandler: ResponseErrorHandler {
+    enum ResponseError: Int, ResponseErrorType {
+        case accessTokenIsValid = 409
+        case expirationRefreshToken = 418
+        
+        func retry(endpoint: Requestable, completion: @escaping (RetryResult) -> Void) {
+            switch self {
+            case .accessTokenIsValid:
+                completion(.notRetryNotPass)
+            case .expirationRefreshToken:
+                NotificationCenter.default.post(name: .init("expirationRefreshToken"), object: nil)
+                completion(.notRetryNotPass)
+            }
+        }
+    }
+    
+    func mappingStatusCode(statusCode: Int) -> ResponseErrorType? {
+        if let response = TokenErrorHandler().mappingStatusCode(statusCode: statusCode) {
+            return response
+        }
+        return ResponseError(rawValue: statusCode)
+    }
+}
