845790120a756126caf9968fe5004812fb6fe7ec
Kim junhwan <tazanboy1@naver.com>
2023-12-12 23:15:14 +0900

    [feat] 로그아웃 Usecase구현

--- LowServiceLevelProject/Domain/UseCase/LoginFlowUseCase.swift
+++ LowServiceLevelProject/Domain/UseCase/LoginFlowUseCase.swift
@@ -6,3 +6,43 @@
 //
 
 import Foundation
+
+protocol LoginFlowUseCase {
+    func login(email: String, password: String, completion: @escaping (Error?) -> Void)
+    func logout() throws
+}
+
+final class DefaultLoginFlowUseCase {
+    let loginRepository: AuthorizationRepository
+    let tokenRepository: TokenRepository
+    
+    init(loginRepository: AuthorizationRepository, tokenRepository: TokenRepository) {
+        self.loginRepository = loginRepository
+        self.tokenRepository = tokenRepository
+    }
+}
+
+extension DefaultLoginFlowUseCase: LoginFlowUseCase {
+    
+    func login(email: String, password: String, completion: @escaping (Error?) -> Void) {
+        loginRepository.login(request: .init(email: email, password: password)) { [weak self] result in
+            switch result {
+            case .success(let success):
+                do {
+                    try self?.tokenRepository.saveToken(tokenCase: .accessToken, value: success.accessToken)
+                    try self?.tokenRepository.saveToken(tokenCase: .refreshToken, value: success.refreshToken)
+                    completion(nil)
+                } catch {
+                    completion(error)
+                }
+            case .failure(let failure):
+                completion(failure)
+            }
+        }
+    }
+    
+    func logout() throws {
+        try tokenRepository.deleteToken(tokenCase: .accessToken)
+        try tokenRepository.deleteToken(tokenCase: .refreshToken)
+    }
+}
