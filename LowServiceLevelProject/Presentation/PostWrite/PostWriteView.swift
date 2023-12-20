58c2134b4d2280fa594e6153e30323f23e3d7180
Kim junhwan <tazanboy1@naver.com>
2023-12-20 22:27:44 +0900

    [feat] 게시글 작성 화면 구현 및 firstResponder 설정

--- LowServiceLevelProject/Presentation/PostWrite/PostWriteView.swift
+++ LowServiceLevelProject/Presentation/PostWrite/PostWriteView.swift
@@ -8,11 +8,46 @@
 import SwiftUI
 
 struct PostWriteView: View {
+    
+    @FocusState private var isFocused: Bool
+    @StateObject var viewModel: PostWriteViewModel = .init()
+    
     var body: some View {
         NavigationStack {
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-                .navigationTitle("게시글 작성")
-                .navigationBarTitleDisplayMode(.inline)
+            HStack {
+                Image(systemName: "person")
+                    .resizable()
+                    .scaledToFill()
+                    .clipShape(Circle())
+                    .frame(width: 30, height: 30)
+                Spacer(minLength: 20)
+                TextField("무슨 일이 일어나고 있나요?", text: $viewModel.content)
+                    .frame(width: .infinity)
+                    .focused($isFocused)
+            }
+            .padding()
+            .navigationTitle("게시글 작성")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Button("취소") {
+                        
+                    }
+                }
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button("게시하기") {
+                        
+                    }
+                    .buttonStyle(.borderedProminent)
+                    .tint(.blue)
+                    .clipShape(Capsule())
+                    .bold()
+                }
+            }
+            Spacer()
+        }
+        .onAppear {
+            isFocused = true
         }
     }
 }
---
+++ LowServiceLevelProject/Presentation/PostWrite/PostWriteViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  PostWriteViewModel.swift
+//  LowServiceLevelProject
+//
+//  Created by JunHwan Kim on 2023/12/20.
+//
+
+import Foundation
+
+final class PostWriteViewModel: ObservableObject {
+    @Published var content: String = ""
+}
