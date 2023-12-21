fc867340d1eaaae22d186b6a9309d9ad960bb3ac
Kim junhwan <tazanboy1@naver.com>
2023-12-21 23:55:19 +0900

    [feat] 글 작성 화면에 커스텀 텍스트 뷰 추가

--- LowServiceLevelProject/Presentation/PostWrite/PostWriteView.swift
+++ LowServiceLevelProject/Presentation/PostWrite/PostWriteView.swift
@@ -9,21 +9,23 @@ import SwiftUI
 
 struct PostWriteView: View {
     
-    @FocusState private var isFocused: Bool
+    @State private var textFieldFirstResponder = true
     @StateObject var viewModel: PostWriteViewModel = .init()
+    @FocusState private var isFocus: Bool
     
     var body: some View {
         NavigationStack {
-            HStack {
+            HStack(alignment: .top) {
                 Image(systemName: "person")
                     .resizable()
                     .scaledToFill()
                     .clipShape(Circle())
                     .frame(width: 30, height: 30)
+                    .padding(.top, 5)
                 Spacer(minLength: 20)
-                TextField("무슨 일이 일어나고 있나요?", text: $viewModel.content)
-                    .frame(width: .infinity)
-                    .focused($isFocused)
+                CustomTextView(text: $viewModel.content, placeholder: "무슨 일이 일어나고 있나요?", placeholderColor: .gray)
+                    .frame(minHeight: 30.0 ,maxHeight: .infinity)
+                    .focused($isFocus)
             }
             .padding()
             .navigationTitle("게시글 작성")
@@ -44,10 +46,10 @@ struct PostWriteView: View {
                     .bold()
                 }
             }
+            .onTapGesture {
+                isFocus = false
+            }
             Spacer()
-        }
-        .onAppear {
-            isFocused = true
         }
     }
 }
