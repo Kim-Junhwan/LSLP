b1c720463665ffe73b5949ffe1b75a22c610ebbd
Kim junhwan <tazanboy1@naver.com>
2023-12-23 23:37:21 +0900

    [refactor] ResponseErrorHandler title과 description 핸들링 할 수 있도록 개선

--- LowServiceLevelProject/Network/ResponseErrorHandler.swift
+++ LowServiceLevelProject/Network/ResponseErrorHandler.swift
@@ -8,13 +8,22 @@
 import Foundation
 
 enum RetryResult {
-    case notRetry(error: Error)
-    case retry(endpoint: Requestable, maxCount: Int)
+    case notRetry(title: String, errorDecoding: ErrorDecoding)
+    case retry(endpoint: Requestable, title: String, errorDecoding: ErrorDecoding, maxCount: Int = 1)
     case notRetryNotPass
-    case retryNotPass(endpoint: Requestable, maxCount: Int)
+    case retryNotPass
 }
 
-protocol ResponseErrorType: LocalizedError {
+enum ErrorDecoding {
+    case localized(description: String)
+    case decoding(decoding: DecodingErrorType.Type)
+}
+
+protocol DecodingErrorType: Decodable {
+    func description() -> String
+}
+
+protocol ResponseErrorType: Error {
     func retry(endpoint: Requestable, completion: @escaping(RetryResult) -> Void)
 }
 
